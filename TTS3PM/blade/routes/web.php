c7a595fd87a8b8e1f282234ff18f1c508f070174
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2023-09-13 17:21:00 +0500

    19-Sep-2023

--- TTS3PM/blade/routes/web.php
+++ TTS3PM/blade/routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+function getUsers() {
+    return [
+        1 => ['name'=>"Taha Tehseen", "city"=>"Karachi", "gender"=>"male"],
+        2 => ['name'=>"Asad Azam", "city"=>"Islamabad", "gender"=>"male"],
+        3 => ['name'=>"Tooba Jawaid", "city"=>"Karachi", "gender"=>"female"],
+        4 => ['name'=>"Saad Khurram", "city"=>"Karachi", "gender"=>"male"],
+        5 => ['name'=>"Hamza Ali Shah", "city"=>"Karachi", "gender"=>"male"]
+    ];
+}
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,14 +24,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 })->name('home');
-Route::get("/about",function () {
+
+Route::get('/about', function () {
     return view('about');
 })->name('about');
-Route::get("/shop",function () {
+
+Route::get('/shop', function () {
     return view('shop');
 })->name('shop');
-Route::get("/contact",function () {
+
+Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
+
+Route::get('/product', function () {
+    return view('product');
+})->name('product');
+
+Route::get('/users', function () {    
+    // $name = "Ali Raza";
+
+    // return view('users',["user" => $name, "city" => "Karachi"]);
+    $students = getUsers();
+
+    return view('users',['users' => $students]);
+})->name('users');
+
+Route::get('/user/{id}', function ($id) {
+    $students = getUsers();
+    $student = $students[$id];
+    return view('user',["user" => $student]);
+})->name('user');
